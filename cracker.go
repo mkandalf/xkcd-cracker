a5889cd1abcbf0f223b497cea8b2d7e1cd05fd52
Nick Meyer <nicholasmeyer@gmail.com>
2013-04-02 01:42:33 -0400

    more processors

--- cracker.go
+++ cracker.go
@@ -10,6 +10,7 @@ import (
   "net/http"
   "net/url"
   "bytes"
+  "runtime"
 )
 
 const goal string = "5b4da95f5fa08280fc9879df44f418c8f9f12ba424b7757de02bbdfbae0d4c4fdf9317c80cc5fe04c6429073466cf29706b8c25999ddd2f6540d4475cc977b87f4757be023f19b8f4035d7722886b78869826de916a79cf9c94cc79cd4347d24b567aa3e2390a573a373a48a5e676640c79cc70197e1c5e7f902fb53ca1858b6"
@@ -66,6 +67,7 @@ func run() {
   }
 }
 func main() {
+  runtime.GOMAXPROCS(8)
   for i := 0; i < 7; i++ {
     go run()
   }
